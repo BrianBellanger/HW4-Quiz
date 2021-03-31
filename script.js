0e5a704b63856beee9a28a2449c52cae68dea959
Brian Bellanger <brianjbellanger@gmail.com>
2021-03-30 22:22:20 -0500

    Second

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var result = document.getElementById("result");
 var secondsLeft = 100;
 var correct=0;
 var incorrect=0;
-var i=0;
+var index=0;
 var finalScore = 0;
 
 //Array to hold question Objects
@@ -77,88 +77,90 @@ function startQuiz() {
 
 //Function to present questions and responses
 function startQuestions() {
-    console.log("i = " + i);
+    console.log("i = " , index);
     //console.log(questions.length);
     //End the Quiz if the last question is done
-    if(i>questions.length) {endQuiz()};
+    if(index>questions.length) {endQuiz()};
 
     //Blank out result
     result.textContent = "";
 
     //Present the question
-    questionText.textContent = questions[i].text;
-
+    questionText.textContent = questions[index].text;
+    
     //Present response A with listener
-    responseA.textContent = questions[i].A;
-    var answA = questions[i].answer;
+    responseA.textContent = questions[index].A;
+    var answA = questions[index].answer;
     responseA.addEventListener("click", function() {
       if (answA==="A") {
           result.textContent = "Correct!";
           correct=correct+10;
-          i++;
-          setTimeout(startQuestions, 3000);
+          //i++;
+          // setTimeout(startQuestions, 3000);
       } else {
         result.textContent = "Wrong!";
         incorrect=incorrect+10;
-        i++;
+        //i++;
         secondsLeft = secondsLeft - 10;
-        setTimeout(startQuestions, 3000);
+        // setTimeout(startQuestions, 3000);
       };
     });
 
     //Present response B with listener
-    responseB.textContent = questions[i].B;
-    var answB = questions[i].answer;
+    responseB.textContent = questions[index].B;
+    var answB = questions[index].answer;
     responseB.addEventListener("click", function() {
       if (answB==="B") {
           result.textContent = "Correct!";
           correct=correct+10;
-          i++;
-          setTimeout(startQuestions, 3000);
+          //i++;
+          // setTimeout(startQuestions, 3000);
       } else {
         result.textContent = "Wrong!";
         incorrect=incorrect+10;
         secondsLeft = secondsLeft - 10;
-        i++;
-        setTimeout(startQuestions, 3000);
+        //i++;
+        // setTimeout(startQuestions, 3000);
       };
     });
 
     //Present response C with listener
-    responseC.textContent = questions[i].C;
-    var answC = questions[i].answer;
+    responseC.textContent = questions[index].C;
+    var answC = questions[index].answer;
     responseC.addEventListener("click", function() {
       if (answC==="C") {
           result.textContent = "Correct!";
           correct=correct+10;
-          i++;
-          setTimeout(startQuestions, 3000);
+          //i++;
+          // setTimeout(startQuestions, 3000);
       } else {
         result.textContent = "Wrong!";
         incorrect=incorrect+10;
         secondsLeft = secondsLeft - 10;
-        i++;
-        setTimeout(startQuestions, 3000);
+        //i++;
+        // setTimeout(startQuestions, 3000);
       };
     });
 
     //Present response D with listener
-    responseD.textContent = questions[i].D;
-    var answD = questions[i].answer;
+    responseD.textContent = questions[index].D;
+    var answD = questions[index].answer;
     responseD.addEventListener("click", function() {
       if (answD==="D") {
           result.textContent = "Correct!";
           correct=correct+10;
-          i++;
-          setTimeout(startQuestions, 3000);
+          //i++;
+          // setTimeout(startQuestions, 3000);
       } else {
         result.textContent = "Wrong!";
         incorrect=incorrect+10;
         secondsLeft = secondsLeft - 10;
-        i++;
-        setTimeout(startQuestions, 3000);
+        //i++;
+        // setTimeout(startQuestions, 3000);
       };
     });
+   index++;
+
 
 };  //End startQuestions() Function
 
@@ -185,9 +187,10 @@ function endQuiz(){
 
 
 function highScores() {  
-    
+  var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
+  if (lastGrade !== null) {
 
+
+}
 
 };  //End highScores() Function
-
-
